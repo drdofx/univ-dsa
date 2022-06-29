f6d66ebefa9b1e37f39a1cb0b18c64b567db3562
Daffa Arviano <daffa.arviano@outlook.com>
2022-06-29 23:36:33 +0700

    add more practice

--- graph-tree/prim-practice.py
+++ graph-tree/prim-practice.py
@@ -8,11 +8,17 @@ V = 5
 index_to_vertex = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'}
 # create a 2d array of size 6x6
 # for adjacency matrix to represent graph
-G = [[0, 5, 0, 2, 0],
-     [6, 0, 3, 0, 0],
-     [0, 0, 0, 0, 12],
-     [0, 0, 12, 0, 7],
-     [0, 14, 0, 0, 0]]
+# G = [[0, 5, 0, 2, 0],
+#      [6, 0, 3, 0, 0],
+#      [0, 0, 0, 0, 12],
+#      [0, 0, 12, 0, 7],
+#      [0, 14, 0, 0, 0]]
+
+G = [[0, 5, 0, 4, 0],
+     [5, 0, 3, 0, 12],
+     [0, 3, 0, 8, 6],
+     [4, 0, 8, 0, 3],
+     [0, 12, 6, 3, 0]]
 
 # create a array to track selected vertex
 # selected will become true otherwise false
